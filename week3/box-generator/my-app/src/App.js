42a038851a2ed79ae0146e5c80507a0c1ad9bd33
Bryan <bryanpablo472@gmail.com>
2023-03-22 09:34:14 -0700

    box-generator practice assignment finished

---
+++ week3/box-generator/my-app/src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+import React, { useState } from 'react';
+import Form from './components/Form';
+import Display from './components/Display';
+
+
+function App() {
+  const [ boxColorArray, setBoxColorArray ] = useState([]);
+
+
+
+  return (
+    <div className="App">
+      <h1>Box Regenerator</h1>
+      <Form boxColorArray={ boxColorArray } setBoxColorArray={ setBoxColorArray } />
+      <Display boxColorArray={ boxColorArray } />
+     
+    </div>
+  );
+}
+
+export default App;
