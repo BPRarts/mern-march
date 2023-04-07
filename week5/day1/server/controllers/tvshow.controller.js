88b399011d3111f4f2bbd8b48792ca55f6cfb17b
Bryan <bryanpablo472@gmail.com>
2023-04-07 16:04:33 -0700

    get one,update,delete, and validations practice

--- week5/day1/server/controllers/tvshow.controller.js
+++ week5/day1/server/controllers/tvshow.controller.js
@@ -1,5 +1,5 @@
 const Show = require('../models/tvshow.model');
- 
+
 module.exports.findAllShows = (req, res) => {
     Show.find()
         .then((allShows) => {
@@ -9,12 +9,52 @@ module.exports.findAllShows = (req, res) => {
             res.json({ message: 'Something went wrong', error: err })
         });
 }
-module.exports.createShow = (req,res) =>{
+module.exports.createShow = (req, res) => {
     Show.create(req.body)
         .then((newShow) => {
             res.json({show:newShow})
         })
-        .catch((err) =>{
-            res.json({message:'something went wrong',error: err})
+        .catch((err) => {
+            res.json({ message: 'Something went wrong', error: err })
         });
+}
+
+module.exports.findOneShow = (req,res) =>{
+    console.log(req.params);
+    Show.findOne({_id: req.params.id})
+        .then((oneShow) => {
+            res.json({show: oneShow})
+        })
+
+        .catch((err) => {
+            res.json({ message: 'Something went wrong', error: err })
+        });
+}
+
+
+
+module.exports.updateShow = (req,res) => {
+    Show.findOneAndUpdate({_id:req.params.id}, req.body,{new:true,runValidators:true})
+        .then((updatedShow) =>{
+            res.json({show:updatedShow}) 
+        })
+        .catch((err) => {
+            res.json({ message: 'Something went wrong', error: err })
+        });
+}
+//updated the office:"EDITED" ans year to 2004
+
+
+
+
+// deleted show:"jackass"
+module.exports.deleteShow = (req,res) => {
+    Show.deleteOne({_id:req.params.id})
+        .then((result) =>{
+            res.json({result:result})
+        })
+        .catch((err) => {
+            res.json({ message: 'Something went wrong', error: err })
+        });
+
 }
--- week5/day1/server/models/tvshow.model.js
+++ week5/day1/server/models/tvshow.model.js
@@ -1,30 +1,38 @@
 const mongoose = require('mongoose');
+//validations goes in here!
  
 const TvShow = new mongoose.Schema({
-
     title: {
-        type: String
+        type: String,
+        required:[true,'Title is required'],
+        minLength:[3,'The title must be 3 or more characters'],
+        maxlenth:[10,'The title is too long']
     },
     releaseYear: {
-        type: Number
+        type: Number,
+        min:[1920,'no shows before 1920']
     },
+    network: {
+        type:String,
+        required:[true,'Network is required'],
+        minLength:[3,'The network must be 3 or more characters'],
+        maxlenth:[10,'The network is too long']
 
-    network:{
-        type:String
     },
-
-    creator: {
-        type: String
+    creator:{
+        type:String,
+        required:[true,'Creator is required'],
+        minLength:[3,'The creator must be 3 or more characters'],
+        maxlenth:[10,'The creator is too long']
     },
-
-    genre: {
-        type:String
+    genre:{
+        type: String,
+        required:[true,'Genre is required'],
+        minLength:[3,'The genre must be 3 or more characters'],
+        maxlenth:[10,'The genre is too long']
     }
-
-
-
 });
- 
+
 const Show = mongoose.model('Show', TvShow);
- 
+
 module.exports = Show;
