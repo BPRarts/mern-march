b4051c36911a0ba7fa3fc1e60109fec845f302c8
Bryan <bryanpablo472@gmail.com>
2023-04-10 12:26:32 -0700

    jokes-api assingment completed

--- week5/jokes-api/server/server.js
+++ week5/jokes-api/server/server.js
@@ -8,4 +8,4 @@ app.use(express.json(), express.urlencoded({ extended: true }));
 const JokeRoutes = require("./routes/joke.routes");
 JokeRoutes(app);
     
-app.listen(8001, () => console.log("The server is all fired up on port 8000"));
+app.listen(8000, () => console.log("The server is all fired up on port 8000"));
