9a94acaa9f9db3aacfec3c9d59f0d68573d0f7da
Bryan <bryanpablo472@gmail.com>
2023-04-10 12:27:04 -0700

    completed

--- week5/jokes-api/server/controllers/joke.controllers.js
+++ week5/jokes-api/server/controllers/joke.controllers.js
@@ -10,10 +10,12 @@ module.exports.findAllJokes= (req, res) => {
         });
 }
 
+
+
 module.exports.createJoke = (req,res) => {
     Joke.create(req.body)
     .then((newJoke)=> {
-        res.json({joke:newJoke})
+        res.json({jokes:newJoke})
     })
     .catch((err) => {
         res.json({ message: 'Something went wrong', error: err })
@@ -21,3 +23,38 @@ module.exports.createJoke = (req,res) => {
 
 }
 
+
+module.exports.findOneJoke = (req,res) => {
+    console.log(req.params);
+    Joke.findOne ({_id:req.params.id})
+         .then((oneJoke) =>{
+            res.json({oneJoke})
+         })
+         .catch((err) => {
+            res.json({ message: 'Something went wrong', error: err })
+        });
+}
+
+
+module.exports.updateJoke = (req,res) => {
+    Joke.findOneAndUpdate({_id:req.params.id}, req.body,{new:true, runValidators:true})
+        .then((updateJoke) => {
+            res.json({Joke:updateJoke})
+        })
+        .catch((err) => {
+            res.json({ message: 'Something went wrong', error: err })
+        });
+
+}
+
+module.exports.deleteJoke = (req,res) => {
+    Joke.deleteOne({_id: req.params.id})
+        .then((result) => {
+            res.json({result:result})
+        })
+
+        .catch((err) => {
+            res.json({ message: 'Something went wrong', error: err })
+        });
+
+}
--- week5/jokes-api/server/routes/joke.routes.js
+++ week5/jokes-api/server/routes/joke.routes.js
@@ -4,4 +4,7 @@ const JokeController = require('../controllers/joke.controllers');
 module.exports = app => {
     app.get('/api/allJokes', JokeController.findAllJokes);
     app.post('/api/newJoke', JokeController.createJoke);
+    app.get('/api/oneJoke/:id', JokeController.findOneJoke);
+    app.put('/api/updateJoke/:id',JokeController.updateJoke);
+    app.delete('/api/deleteJoke/:id', JokeController.deleteJoke);
 }
