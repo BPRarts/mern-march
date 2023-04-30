a01e1c5c4623647bd03a070aa92d9772c476524a
Bryan <bryanpablo472@gmail.com>
2023-04-30 16:43:46 -0700

    bootstrap setup

---
+++ week8/using-bootstrap/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav class="navbar bg-body-tertiary">
+          <div class="container-fluid">
+          <span class="navbar-brand mb-0 h1">Navbar</span>
+          </div>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
